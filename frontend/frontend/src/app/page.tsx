4f27df350c75a268432e45efa26822cf88dacd68
munish-shah-tamu <142832308+munish-shah-tamu@users.noreply.github.com>
2025-01-25 19:39:27 -0600

    Color Change

--- frontend/frontend/src/app/page.tsx
+++ frontend/frontend/src/app/page.tsx
@@ -1,16 +1,17 @@
 import Link from "next/link";
 
-export default function HomePage() {
+export default async function HomePage() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+        <h1 className="text-5xl font-extrabold tracking-tight text-black sm:text-[5rem]">
           Counter
         </h1>
         
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-gray-100 p-4 text-black hover:bg-gray-200"
             href="/features"
           >
             <h3 className="text-2xl font-bold">About →</h3>
@@ -20,7 +21,7 @@ export default function HomePage() {
           </Link>
           
           <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-gray-100 p-4 text-black hover:bg-gray-200"
             href="/get-started"
           >
             <h3 className="text-2xl font-bold">Get Started →</h3>
